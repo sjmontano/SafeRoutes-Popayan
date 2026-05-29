cd278864456dc63f4f6ec36e462c26f2b75419c0
sjmontano <smontano@unimayor.edu.co>
2026-05-28 21:27:40 -0500

    fix: API apunta a Render (https://saferoutes-popayan.onrender.com/api)

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:3001/api';
+const API = 'https://saferoutes-popayan.onrender.com/api';
 
 export async function fetchGraphData() {
   const res = await fetch(`${API}/graph`);
